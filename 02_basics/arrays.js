34cf545844e22aac2291cc950c0dacdf1dbf7ac6
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-18 06:42:11 +0000

    arrrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,25 @@
+const array=[5,9,4,8,7]
+console.log(array)
+
+array.push(1)
+
+console.log(array)
+
+array.shift(9)
+
+console.log(array)
+
+
+
+array.pop(7)
+console.log(array)
+
+const newarray=array.join()
+console.log(newarray)
+console.log(typeof(newarray))
+console.log(typeof(array))
+
+
+console.log(array.indexOf(8))
+console.log(array.includes(9))
+console.log(array[3])
