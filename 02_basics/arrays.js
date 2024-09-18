ec08ffd720650a0bc265cfe315a113eadd556597
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-18 07:00:37 +0000

    arrays

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -23,3 +23,19 @@ console.log(typeof(array))
 console.log(array.indexOf(8))
 console.log(array.includes(9))
 console.log(array[3])
+
+
+const mynewarray= [0,1,2,3,4,5]
+console.log("A  " , mynewarray)
+
+const mn1=mynewarray.slice(1,3)
+
+console.log("B  " , mn1)
+console.log(  mynewarray)
+
+
+
+const mn2=mynewarray.splice(0,2)
+
+console.log("C  ", mn2)
+console.log( mynewarray)
