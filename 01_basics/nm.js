53a898da7e2d7e2b832ff1b507fe0bc87269438d
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-18 05:14:55 +0000

    nm

---
+++ 01_basics/nm.js
@@ -0,0 +1,13 @@
+let numb=400
+let num= new Number("100")
+
+console.log(numb,num)
+
+console.log(num.toString())
+
+const decimal=100.23694444
+console.log(decimal.toFixed(2))
+
+console.log(decimal.toPrecision(6))
+let bignumber=10000000
+console.log(bignumber.toLocaleString())
