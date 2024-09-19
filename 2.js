b4cdddde15041d78fded47d303c41a3d5a210383
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-19 09:30:42 +0500

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,13 @@
+const array=[1,2,3,4,5]
+console.log(array)
+
+array.unshift(9)
+console.log(array)
+array.shift()
+console.log(array)
+
+array.push(10)
+console.log(array)
+
+array.pop()
+console.log(array)
---
+++ consandvar.js
@@ -0,0 +1,13 @@
+const accountid="1122"
+let accountemail="xyz@gmail.com"
+let password="Khan34324"
+let accountcity="Islamabad"
+let accountstate;
+
+accountemail="mansurkhann@gmail.ali"
+accountcity="Rawalpindi"
+accountstate;
+password="Khalidkhann1"
+
+console.log(accountid);
+console.table([accountid,accountemail,accountcity,accountemail,accountstate,password])
