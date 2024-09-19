45b1a02689fec99b7eb5f05c8fcdb614c16f1168
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-19 08:14:48 +0000

    mm

---
+++ 03_Functions/blockscope and global scope.js
@@ -0,0 +1,7 @@
+/// we should not declare variables by var because it comes out of the scope
+///scopes are curly braces in the functions or conditions.
+/// there are two types of scopes global and block
+////the things inside the braces are block and outside the braces are global scopes
+//// always declare variable by let or const(cannot be changed)
+/// declaration of variable by var will make it difficult to debug becaue it will take value even outside the block
+//// if we declare with let it will not come out of the block or effect the remaining coding
