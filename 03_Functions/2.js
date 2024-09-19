c08e0f78b712277858d6f26d0a2c502c8b2de0dd
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-19 07:36:24 +0000

    functions in object and arrays

---
+++ 03_Functions/2.js
@@ -0,0 +1,37 @@
+function shoppingcart(...sahil){
+
+    return sahil
+
+}
+
+
+console.log("THe shopping cart is",shoppingcart(200,300,400))
+
+
+/// array in objects//
+
+
+const user={
+    name:"Khalid",
+    class:2,
+    Price:50000
+}
+
+const user2={
+    name:"Sahil",
+    class:3,
+    Price:10000
+}
+
+function handleObject(anyobject){
+    console.log(`the name is ${anyobject.name} and the price is ${anyobject.Price}`);}
+
+handleObject(user)
+handleObject(user2)
+
+////
+
+
+
+
+
