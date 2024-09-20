38993502e8f0c59d58eaa2754b146f5d8d58bad8
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-20 07:12:44 +0000

    nullish coalescing and ternary operators

---
+++ 04_Controlflow/Nullish coalescing&Ternary Operators.js
@@ -0,0 +1,28 @@
+///Nulish colecieng operator :Null,undefined
+let a;
+a=5??20
+
+console.log(a)
+
+let b;
+b=null??20
+console.log(b)
+
+
+
+let c;
+c=undefined??50
+
+console.log(c)
+
+let d;
+d=null??10??20
+
+
+
+
+///Ternary operator
+
+let priceicetea=50
+
+priceicetea<=80?console.log("Less than 80 "):console.log("Greater than 80")
