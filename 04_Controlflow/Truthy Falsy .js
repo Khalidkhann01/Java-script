dbcee25b5e8c0a247385572f512c522676084667
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-20 06:57:10 +0000

    Truthy falsy

---
+++ 04_Controlflow/Truthy Falsy .js
@@ -0,0 +1,23 @@
+const useremail="mansoor@gmail.com"
+const array=[]
+const emptyobject={}
+
+
+
+if(useremail){
+    console.log("Have user email")
+}
+else{
+    (console.log("No user email"))}
+
+
+
+    ///Falsy Values
+    //false :: 0 ,-0,BigInt 0N,"" ,Null,Undefined,NaN
+
+        ///Truthy values 
+         ////True Values :: "0" ,'false'," ",[],{},function(){} emptyfunction,
+
+if(array.length===0){console.log("Array is Empty")}
+
+if(Object.keys(emptyobject).length===0){console.log("Object is empty")}
