0878a04c863a57c0d4bd9b41b1719c9ded371e83
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-21 06:19:36 +0000

    while and do while loop

---
+++ 05_Iterations/While and DOWHILE.js
@@ -0,0 +1,25 @@
+let value=0
+
+while (value<=5) {
+    console.log(`Value is ${value}`)
+     value=value+2
+    
+}
+
+
+
+
+const myarray=["Khalid","Umer","Sahil","Ali"]
+let arr=0
+
+while (arr<=myarray.length) { console.log(`The name is ${myarray[arr]}`)
+    arr=arr+1
+    
+}
+
+let score=1005
+do {
+    console.log(`The score is ${score}`)
+    score++
+    
+} while (score<=50);
