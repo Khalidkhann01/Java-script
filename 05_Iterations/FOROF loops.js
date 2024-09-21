8d8a39799b429afa49ff6f557c5b4ebf93b0b469
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-21 06:44:46 +0000

    forofloops on arrays object and maps

---
+++ 05_Iterations/FOROF loops.js
@@ -0,0 +1,23 @@
+let greetings="HEllo world how are you"
+
+for (const greet of greetings) { console.log(`Value is ${greet}`)
+    
+}
+
+
+const myarray=[10,15,20,30,4,50]
+for (const num of myarray) { console.log(`The key of array is ${num}`)
+    
+}
+
+
+const map= new Map()
+map.set("IN","India") 
+map.set("US","America") 
+map.set("PK","Pakistan")           //////MAP is an object it holds unique values no repeat in it.
+console.log(map)
+
+
+for (const [key,values] of map) {
+    console.log(`${key}::${values}`)
+}
