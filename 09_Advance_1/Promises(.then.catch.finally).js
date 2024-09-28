90fc4d55eb7215ef88ae3e148234865759bbefda
Khalidkhann01 <kkhalid60@gmail.com>
2024-09-28 07:49:07 +0000

    fetch

--- 09_Advance_1/Promises(.then.catch.finally).js
+++ 09_Advance_1/Promises(.then.catch.finally).js
@@ -73,4 +73,61 @@ const promise4=new Promise(function(resolve,reject){
             console.log("Error is either resolved or removed")
         })
     
-    
+        const promise= new Promise(function(resolve,reject){
+            let error=false
+            setTimeout(function() {
+                
+            
+            if(!error){
+                resolve({Language:"Java Script", Code:'112233'})
+            }
+            else{
+                reject('JS Went Wrong')
+            }
+            },1000)
+            })
+            
+            async function consumedpromise() {
+                
+                try {
+                    const response= await promise
+                console.log(response)
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+            consumedpromise()
+        
+        
+            async function getallusers() {
+                try {
+                    const response= await fetch('https://api.github.com/users/hiteshchoudhary')
+                    const data= await response.json()             ///by async await 
+                    console.log(data)
+                    
+                } catch (error) {
+                    console.log("Error")
+                    
+                }
+            }
+        getallusers()
+        
+        
+        
+        fetch('https://api.github.com/users/hiteshchoudhary')           ///by .then method
+        .then(function(response){
+        
+        return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+        })
+        .catch(()=>{
+            console.log('Error')
+        }).finally(function () {
+            console.log("Either done or not")
+            
+        })
+        
+        
+            
